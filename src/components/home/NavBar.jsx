aee5e852bc3c18df8a59fb7ccc4dabc88a4abb04
imanee11 <imaneelouannane@gmail.com>
2025-04-29 17:51:53 +0100

    add imane branch

--- src/components/home/NavBar.jsx
+++ src/components/home/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar = () => {
     return (
         <>
-        <h1 className='text-red-400'>Hii</h1>
+        <h1 className='text-red-400'>Hello</h1>
             
         </>
     );
