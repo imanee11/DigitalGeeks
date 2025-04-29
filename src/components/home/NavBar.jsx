b08bde76116840af49d75ab73a30803c4796ade8
imanee11 <imaneelouannane@gmail.com>
2025-04-29 16:42:21 +0100

    first commit

---
+++ src/components/home/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <>
+        <h1 className='text-red-400'>Hii</h1>
+            
+        </>
+    );
+};
+
+export default NavBar;
