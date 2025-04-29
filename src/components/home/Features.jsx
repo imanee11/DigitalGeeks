82ee851da930b62c781ca1c43c15c27bc8e0e0fd
imanee11 <imaneelouannane@gmail.com>
2025-04-29 18:10:27 +0100

    add images folder

---
+++ src/components/home/Features.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Features = () => {
+    return (
+        <>
+            <p className='font-bold'>Features</p>
+
+            
+        </>
+    );
+};
+
+export default Features;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../components/home/NavBar';
+import Features from '../components/home/Features';
 
 const Home = () => {
     return (
         <>
-        <NavBar/>
+        {/* <NavBar/> */}
+            <Features/>
             
         </>
     );
