2b45124d65153fceedb55df1689afec0aae434eb
imanee11 <imaneelouannane@gmail.com>
2025-04-30 05:07:28 +0100

    add logo to the second navbar

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -11,6 +11,10 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useNavigate } from 'react-router-dom';
 import Footer from './home/Footer';
 
+import logo from '../assets/images/image-removebg-preview (2).png'
+
+
+
 
 
 
@@ -103,9 +107,13 @@ const Blog = () => {
       {/* navbar */}
       <header className={`${darkMode ? "bg-black text-white" : "bg-white text-gray-900"} shadow-md`}>
         <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
-          <a className="text-2xl font-bold tracking-wide cursor-pointer" onClick={() => navigate("/")}>
-            JobTalk
-          </a>
+          <div className='flex items-center gap-2'>
+            <img src={logo} alt="" className='h-12' />
+            <a className="text-2xl font-bold tracking-wide cursor-pointer" onClick={() => navigate("/")}>
+              JobTalk
+            </a>
+          </div>
+
 
           {/* Desktop Links */}
           {/* <div className="hidden md:flex items-center gap-6">
